4288156ffbe10b2dbf33fdb19acf2b0c94a4e4fc
julnow <jul.nowk@gmail.com>
2023-05-30 14:46:10 +0200

    migrate from jit to njit

--- fasttreeshap/links.py
+++ fasttreeshap/links.py
@@ -1,22 +1,22 @@
 import numpy as np
-import numba
+from numba import njit
 
-@numba.jit
+@njit
 def identity(x):
     """ A no-op link function.
     """
     return x
-@numba.jit
+@njit
 def _identity_inverse(x):
     return x
 identity.inverse = _identity_inverse
 
-@numba.jit
+@njit
 def logit(x):
     """ A logit link function useful for going from probability units to log-odds units.
     """
     return np.log(x/(1-x))
-@numba.jit
+@njit
 def _logit_inverse(x):
     return 1/(1+np.exp(-x))
 logit.inverse = _logit_inverse
